f36f22fe9a3045e36e42b7c5c7311567c14cdc59
sbs29 <32124063+sbs29@users.noreply.github.com>
2024-05-17 18:13:06 +0000

    finish excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const generateExcuse = () => {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  };
+
+  const updateExcuse = () => {
+    const excuseElement = document.getElementById("excuse");
+    excuseElement.innerHTML = generateExcuse();
+  };
+
+  updateExcuse();
+
+  const generateButton = document.getElementById("generateButton");
+  generateButton.addEventListener("click", updateExcuse);
 };
